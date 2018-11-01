30f7298773e0d6a090d1fab5ab1c5b24186a4af3
Georgios Andrianakis <geoand@gmail.com>
2018-11-01 17:50:21 +0200

    Add spring cloud kubernetes sample

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,17 +1,23 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableConfigurationProperties(DummyConfig.class)
 public class Application {
+
+    @Autowired
+    private DummyConfig dummyConfig;
 
     @GetMapping("/")
     public String home() {
-        return "Hello Spring Boot";
+        return dummyConfig.getMessage();
     }
 
     public static void main(String[] args) {
